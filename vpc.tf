39d7aae24eb0afabce9b936a11578f2aa5adca3f
1unarfox <46571573+1unarfox@users.noreply.github.com>
2022-10-03 00:15:56 -0500

    Corrected variable declaration

--- vpc.tf
+++ vpc.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet" {
   count             = var.vpc_cidr == "10.0.0.0/16" ? 3 : 0
   vpc_id            = aws_vpc.lamp_vpc.id
   availability_zone = data.aws_availability_zones.azs.names[count.index]
-  cidr_block        = element(cidrsubnets(var.lamp_vpc, 8, 4, 4), count.index)
+  cidr_block        = element(cidrsubnets(var.vpc_cidr, 8, 4, 4), count.index)
 
   tags = {
     "Name" = "Public-Subnet-${count.index}"
